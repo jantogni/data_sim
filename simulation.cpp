c29801f1dfb25b077395e2cafb52b6bb300e9b02
jantogni <jantogni@csrg.cl>
2013-08-25 21:03:27 -0400

    adding constants

--- simulation.cpp
+++ simulation.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void slist_c(int n, float p){
+void slist_c(int n){
 	int i;
 	star sim_star;
 	list<star> stars;
@@ -10,9 +10,9 @@ void slist_c(int n, float p){
 	srand(time(NULL));
 
 	for(i=0; i<n; i++){
-		sim_star.ra = (double)rand()/((double)RAND_MAX/360);
-		sim_star.dec = ((rand() % 2)*2 - 1)*(double)rand()/((double)RAND_MAX/90);
-		sim_star.magnitude = (float)rand()/((float)RAND_MAX/10);
+		sim_star.ra = (double)rand()/((double)RAND_MAX/DRA);
+		sim_star.dec = ((rand() % 2)*2 - 1)*(double)rand()/((double)RAND_MAX/DDEC);
+		sim_star.magnitude = (float)rand()/((float)RAND_MAX/MAGNITUDE);
 
 		cout << "RA = " << sim_star.ra << "\tDEC = " << sim_star.dec << "\tMag = " << sim_star.magnitude << endl;
 		stars.push_back(sim_star);	
@@ -20,6 +20,6 @@ void slist_c(int n, float p){
 }
 
 int main(){
-	slist_c(10, 10);
+	slist_c(N);
 	return 0;
 }
--- simulation.hpp
+++ simulation.hpp
@@ -6,6 +6,11 @@
 #include<list>
 #include<time.h>
 
+#define N			1000	//number of stars
+#define DRA			360		//right asension degree
+#define DDEC		90		//declination positive degree
+#define MAGNITUDE	10		//star magnitude
+
 /* Star structure definition
 */
 typedef struct star{

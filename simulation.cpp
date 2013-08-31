dde096fa858ae685914e8c99ac0ec73affa6c1b3
jantogni <jantogni@csrg.cl>
2013-08-31 14:48:56 -0400

    adding creation of catalogue 1

--- simulation.cpp
+++ simulation.cpp
@@ -2,7 +2,29 @@
 
 using namespace std;
 
-void slist_c(int n){
+//star_distance calculate distance between s1 and s2
+double star_distance(star s1, star s2){
+	return sqrt( pow(s1.ra - s2.ra,2) + pow(s1.dec - s2.dec, 2) );
+}
+
+//is_feasible check if is possible add the star s to the catalogue stars 
+bool is_feasible(list<star> stars, star s){
+	list<star>::iterator it_star;
+	bool feasibility = true;
+
+	for(it_star = stars.begin(); it_star != stars.end(); it_star++){
+		if(star_distance(*it_star, s) < RES_1){
+			feasibility = false;
+			cout << "Star not possible" << endl;
+			break;
+		}
+	}
+
+	return feasibility;
+}
+
+//slist_c create the first catalogue with n stars and resolution RES_1
+list<star> slist_c1(int n){
 	int i;
 	star sim_star;
 	list<star> stars;
@@ -14,12 +36,26 @@ void slist_c(int n){
 		sim_star.dec = ((rand() % 2)*2 - 1)*(double)rand()/((double)RAND_MAX/DDEC);
 		sim_star.magnitude = (float)rand()/((float)RAND_MAX/MAGNITUDE);
 
-		cout << "RA = " << sim_star.ra << "\tDEC = " << sim_star.dec << "\tMag = " << sim_star.magnitude << endl;
-		stars.push_back(sim_star);	
+
+		if( is_feasible(stars, sim_star) ){
+			cout << "RA = " << sim_star.ra << "\tDEC = " << sim_star.dec << "\tMag = " << sim_star.magnitude << endl;
+			stars.push_back(sim_star);	
+		}
 	}
+
+	return stars;
+}
+
+//TODO: create a region using resolution 2
+void set_region(star * r, int i, int j){
+	r.ra1	= RES_2 * i;
+	r.dec1	= RES_2 * j;
+	
+	r.ra2	= RES_2 * (i+1); 
+	r.dec2	= RES_2 * (j+1);
 }
 
 int main(){
-	slist_c(N);
+	slist_c1(N);
 	return 0;
 }
--- simulation.hpp
+++ simulation.hpp
@@ -5,11 +5,16 @@
 #include<stdlib.h>
 #include<list>
 #include<time.h>
+#include<math.h>
 
 #define N			1000	//number of stars
 #define DRA			360		//right asension degree
 #define DDEC		90		//declination positive degree
 #define MAGNITUDE	10		//star magnitude
+#define RES_1		0.5		//resolution of first catalogue
+#define RES_2		1		//resolution of second catalogue
+
+using namespace std;
 
 /* Star structure definition
 */
@@ -18,5 +23,21 @@ typedef struct star{
 	double dec;
 	double magnitude;
 } star;
+
+typedef struct region{
+	double ra_1;
+	double dec_1;
+
+	double ra_2;
+	double dec_2
+} region;
+
+double star_distance(star s1, star s2);
+
+bool is_feasible(list<star> stars, star s);
+
+list<star> slist_c1(int n);
+
+void set_region(region * r, int i, int j);
 
 #endif
